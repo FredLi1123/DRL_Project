555386898146726285c2ad1dba9d9c457c86d84c
Nuoyu Li <readn@icloud.com>
2018-04-16 21:45:31 -0400

    reinforce updated

--- reinforce.py
+++ reinforce.py
@@ -1,10 +1,15 @@
 # coding: utf-8
-
+# first party
+from model import RNNModel
+# third party
 import sys
 import argparse
 import os
 import random
 import numpy as np
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
 
 
 def _get_action(logits, nactions, testing):
@@ -16,11 +21,22 @@ def _get_action(logits, nactions, testing):
     
     return action
 
+class GaussianNet(nn.Module):
+
+    def __init__(self, sigma):
+        super(GaussianNet, self).__init__()
+        self.sigma = sigma
+    
+    def forward(self, mu, sample):
+        logprob = -1/(2*sigma*sigma) * torch.sum((sample-mu)**2)
+        return logprob
+
 
 class Reinforce(object):
     # Implementation of the policy gradient method REINFORCE.
-    def __init__(self, model, lr=1e-3):
+    def __init__(self, model, sigma, lr=1e-3):
         self.model = model
+        self.sigma = sigma
 
         # TODO: Define any training operations and optimizers here, initialize
         #       your variables, or alternately compile your model here.
@@ -32,7 +48,7 @@ class Reinforce(object):
         # TODO: Implement this method. It may be helpful to call the class
         #       method generate_episode() to generate training data.
         """
-        states, rewards, total_reward = self.generate_episode(testing=False)
+        probs, rewards, total_reward = self.generate_episode(testing=False)
         episode_len = len(rewards)
         
         running_sum = 0.0
@@ -44,51 +60,40 @@ class Reinforce(object):
         loss = returns * log(probs)
         return loss
         """
-        nactions = env.action_space.n
-        # obtain information from an episode
-        states, actions, rewards, training_reward = self.generate_episode(env, render=False)
+        probs, rewards, total_reward = self.generate_episode(testing=False)
         episode_len = len(rewards)
-        rewards = np.array(rewards, dtype='float')
-        returns = np.zeros(episode_len)
-        embedded_returns = np.zeros((episode_len, nactions))
+        
         running_sum = 0.0
+        returns = Variable(torch.zeros(episode_len), requires_grad=False)
         # calculate returns
         for i in range(episode_len-1, -1, -1):
             running_sum = rewards[i] + gamma * running_sum
             returns[i] = running_sum
-        # standardize returns
-        returns = (returns - np.mean(returns)) / np.std(returns)
-        # embed the returns with corresponding actions
-        for i in range(episode_len):
-            embedded_returns[i][actions[i]] = returns[i]
-        # train the model
-        states = np.concatenate(states).reshape(episode_len, -1)
-        self.model.fit(states, embedded_returns, batch_size=episode_len, shuffle=True, epochs=1, verbose=1)
-        # apply weight clipping
-        self.clip_weights(-1.0, 1.0)
         
+        loss = returns * torch.log(probs)
+        total_loss = torch.sum(loss)
+        return total_loss
 
     def evaluate(self, env, test_episodes, render=False):
         # generate episodes and obtain mean and standard deviation of rewards
         records = np.zeros(test_episodes)
         for i in range(test_episodes):
-            _, _, _, reward = self.generate_episode(env, render=render, testing=True)
+            _, _, _, reward = self.generate_episode(env, testing=True)
             records[i] = reward
         
         return np.mean(records), np.std(records)
     
-    def generate_episode(self, testing=False):
+    def generate_episode(self, model, inputs, targets, testing=False):
         # Generates an episode by executing the current policy in the given env.
         # Returns:
         # - a list of states, indexed by time step
         # - a list of actions, indexed by time step
         # - a list of rewards, indexed by time step
         # TODO: Implement this method.
-        states = []
+        probs = []
         actions = []
         rewards = []
-
-        is_terminal = False
+        total_reward = 0.0
 
         """
         :param model - a LSTM-based word language model
@@ -108,31 +113,28 @@ class Reinforce(object):
         
         return probs, rewards, total_reward
         """
+        len_sentence = inputs.size(1)
+        sampler = GaussianNet(self.sigma)
+        hidden = model.init_hidden(1)
+        loss_func = nn.CrossEntropyLoss()
 
-        while not is_terminal:
-            # predict and obtain the next step
-            logits = self.model.predict(cur_state.reshape(1, -1), batch_size=1)
-            action = _get_action(logits[0], nactions, testing=testing)
-            nextstate, reward, is_terminal, _ = env.step(action=action)
-            # record the information
-            states.append(cur_state)
-            actions.append(action)
-            rewards.append(reward/100)
-            # get to the next step
-            cur_state = nextstate
+        for t in range(len_sentence)):
+            output, hidden = model(Variable(torch.from_numpy(x), requires_grad=False), hidden)
+            # execute policy: sample from mean hidden, std sigma
+            std = torch.Tensor(hidden.size()).fill_(self.sigma)
+            next_hidden = Variable(torch.normal(hidden.data, std), requires_grad=False)
+            # calculate log-prob and return as Variable
+            logprob = sampler(hidden, next_hidden)
+            reward = loss_func(output, target[t])
+            
+            total_reward += reward
+            probs.append(logprob)
+            rewards.append(reward)
+
+            hidden = next_hidden
         
-        total_reward = sum(rewards)*100
-        
-        return states, actions, rewards, total_reward
+        return probs, rewards, total_reward
     
-    def sample(h, c, x):
-        """
-        output, h_next, c_next = self.model(h, c, x)
-        output_sample, h_next_sample, c_next_sample = torch.random.normal(mu = (output, h_next, c_next), self.Sigma)
-        probs = torch.distributions.Gaussian(output_sample, mu=output, sigma=self.sigma)
-
-        return probs, output_sample
-        """
 
     def clip_weights(self, lb, ub):
         # fetch current weights
